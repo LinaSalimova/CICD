01820b9f8a2870586a5ec85f06410097784ea271
li <lllinaalekseevall@gmail.com>
2024-12-30 18:27:20 +0300

    Initial commit with Calculator project

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,18 @@
+
+import org.example.Calculator;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+    private final Calculator calculator = new Calculator();
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(1, calculator.subtract(3, 2));
+    }
+}
